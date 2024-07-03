66f536da2d5c694f256ed99daabcf5eccf9ba904
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-07-03 14:19:50 +0200

    update chrome driver

--- app/utils.py
+++ app/utils.py
@@ -11,6 +11,9 @@ def get_driver() -> WebDriver:
 
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
     options.add_argument("--start-maximized")
     options.add_argument("user-agent={}".format(user_agent))
 
